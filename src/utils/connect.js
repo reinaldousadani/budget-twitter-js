292018d668b867bed7974e15562e490963fa733b
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2022-07-20 00:27:40 +0700

    Finished

---
+++ src/utils/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Connected");
+  } catch (error) {
+    console.error("Can't connect to DB");
+    process.exit(1);
+  }
+}
+
+module.exports = connect;
