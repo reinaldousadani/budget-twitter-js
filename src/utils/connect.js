46bbf8330acfd867b46c163e0d014d3956850aaf
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2022-09-26 00:08:00 +0700

    Changed connection URI to local

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(process.env.DB_URI_LOCAL);
     console.log("DB Connected");
   } catch (error) {
     console.error("Can't connect to DB");
